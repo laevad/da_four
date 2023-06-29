4c45bb9014509aa7fe02ee4d626221d0438b6b23
laevad <laevad>
2023-06-29 14:21:02 +0800

    class

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -2,7 +2,7 @@ export default function Circle({ color, handleClick }: { id: number, children: a
   return (
     <div
       onClick={handleClick}
-      className={`h-16 sm:h-20 w-16 sm:w-20 rounded-full border-4 ${color} flex items-center justify-center font-bold border-gray-900/50 hover:border-gray-900/100 cursor-pointer`}
+      className={`h-12 sm:h-16 md:h-20 w-12 sm:w-16 md:w-20 rounded-full border-4 ${color} flex items-center justify-center font-bold border-gray-900/50 hover:border-gray-900/100 cursor-pointer`}
     >
     </div>
   );
